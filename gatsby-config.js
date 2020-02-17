6a1c243747a3ab47844f122841f0b16105563307
ahdvnd <hadavand.a@gmail.com>
2020-02-17 09:55:35 -0500

    change gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "Aboozar hadavand",
-    description: "All my resume in one page",
+    title: "Aboozar Hadavand",
+    description: "Personal Website",
     siteUrl: "https://ahadavand.com",
-    githubRepo: "https://github.com/Eddie-CooRo/ahadavand.com",
-    mail: "mailto:info@pravdomil.com",
-    twitter: "https://twitter.com/pravdomil",
-    github: "https://github.com/pravdomil",
+    githubRepo: "https://github.com/ahdvnd/ahadavand.com",
+    mail: "mailto:hadavand.a@gmail.com",
+    twitter: "https://twitter.com/ahdvnd",
+    github: "https://github.com/ahdvnd",
     paypal:
-      "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=BCL2X3AFQBAP2&item_name=pravdomil.com%20Beer",
-    stackoverflow: "https://stackoverflow.com/users/3748498/pravdomil",
-    youtube: "https://youtube.com/pravdomil",
+      "",
+    stackoverflow: "",
+    youtube: "",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
